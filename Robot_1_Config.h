fb51a61acc7ff145eb6af5345f78b3b1c1e4decf
curtiss <curtiss.porter@comcast.net>
2016-06-09 10:27:51 -0600

    Update Robot_1_Config.h

--- Robot_1_Config.h
+++ Robot_1_Config.h
@@ -1,17 +1,17 @@
 //These three pins control the left motor
-#define LeftMotorDirectionA 7
-#define LeftMotorDirectionB 9
-#define LeftMotorSpeed 8 //This pin connects to the "Enable" pin of the H-Bridge
+#define LeftMotorDirectionA 4
+#define LeftMotorDirectionB 5
+#define LeftMotorSpeed 6 //This pin connects to the "Enable" pin of the H-Bridge
 
 //These three pins control the right motor
-#define RightMotorDirectionA 11
-#define RightMotorDirectionB 13
-#define RightMotorSpeed 12 //This pin connects to the "Enable" pin of the H-Bridge
+#define RightMotorDirectionA 9
+#define RightMotorDirectionB 8
+#define RightMotorSpeed 10 //This pin connects to the "Enable" pin of the H-Bridge
 
-#define LeftDistanceSensor 24
-#define RightDistanceSensor 25
-#define BackDistanceSensor 26
+#define LeftDistanceSensor A5
+#define RightDistanceSensor A6
+#define BackDistanceSensor A7
 
-#define LeftLineSensor 22
-#define RightLineSensor 23
-#define BackLineSensor 21
+#define LeftLineSensor A3
+#define RightLineSensor A4
+#define BackLineSensor A2
